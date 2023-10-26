31207fff2d12028b8cccb870309866a9d5f2d004
umarrazay <umarraza452@gmail.com>
2023-10-26 15:15:14 +0500

    blank react project with backend running

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,6 @@ const url = 'mongodb://localhost:27017/'
 const database = 'react-admin-panel'; 
 const client = new MongoClient(url);
 
-
-
 async function connectToDatabase() {
     try {
         await client.connect();

2063ced5bddd16715e515f6d1a6ec1181b7ab054
umarrazay <umarraza452@gmail.com>
2023-10-26 14:37:27 +0500

    proejct setup

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb://localhost:27017/'
+const database = 'react-admin-panel'; 
+const client = new MongoClient(url);
+
+
+
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+        const db = client.db(database); 
+        console.log(`Using database: ${database}`);
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+connectToDatabase();
